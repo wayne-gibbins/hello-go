37a995fd2b0c40b81f998b0e9b4e696fe9d7b444
mies <mies@wercker.com>
2016-08-23 15:28:01 +0100

    init

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func IndexHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello World"))
+}
+
+func main() {
+	log.Println("starting web server...")
+
+	http.HandleFunc("/", IndexHandler)
+	http.ListenAndServe(":8080", nil)
+}
